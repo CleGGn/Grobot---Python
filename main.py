f40775ad43180f1f78aa13d0b95694a4b00a6d8a
Clément <gaugainc@yahoo.com>
2022-08-08 20:01:13 +0200

    Added commands + calls to JIKANpy API

--- main.py
+++ main.py
@@ -1,36 +1,92 @@
-import discord
+import discord 
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import random
+from ressources.anime import random_anime_digger, search_anime
+import youtube_dl
+from ressources.YTDLSource import YTDLSource
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-bot = discord.Client()
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-
-# EVENT LISTENER FOR WHEN THE BOT HAS SWITCHED FROM OFFLINE TO ONLINE.
 @bot.event
 async def on_ready():
-	# CREATES A COUNTER TO KEEP TRACK OF HOW MANY GUILDS / SERVERS THE BOT IS CONNECTED TO.
-	guild_count = 0
-	print('We have logged in as {0.user}'.format(bot))
-	# LOOPS THROUGH ALL THE GUILD / SERVERS THAT THE BOT IS ASSOCIATED WITH.
-	for guild in bot.guilds:
-		# PRINT THE SERVER'S ID AND NAME.
-		print(f"- {guild.id} (name: {guild.name})")
-		# INCREMENTS THE GUILD COUNTER.
-		guild_count = guild_count + 1
+    print('Connecté en tant que {0.user}'.format(bot))
 
-	# PRINTS HOW MANY GUILDS / SERVERS THE BOT IS IN.
-	print("Grobot est dans " + str(guild_count) + " serveur.")
+async def get_prefix(bot, message):
+    default_prefix= '!'
+    return default_prefix
 
-# EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL.
+@bot.command(
+	brief="Writes pong"
+)
+async def ping(ctx):
+	await ctx.channel.send("pong")
+
+# Jikan Part
+@bot.command(
+    brief= "Fetch a random anime on MyAnimeList"
+)
+async def randanime(ctx):
+    response = random_anime_digger()
+    await ctx.channel.send(response)
+
+@bot.command()
+async def lfanime(ctx, *args):
+	response = search_anime(args)
+	await ctx.channel.send(response)
+
+# Youtube Part
+@bot.command(
+    name='join', help='Appelle le bot dans le canal vocal'
+    )
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("{} n'est pas connecté à un canal vocal".format(ctx.message.author.name))
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+@bot.command(
+    name='leave', help='Renvoie le bot du canal vocal'
+    )
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send("Le bot n'est pas connecté à un canal vocal.")
+
+@bot.command(
+    name='play_song', help='To play song'
+    )
+async def play(ctx,url):
+    try :
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(url, loop=bot.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+        await ctx.send('**Now playing:** {}'.format(filename))
+    except:
+        await ctx.send("The bot is not connected to a voice channel.")        
+
 @bot.event
 async def on_message(message):
-	# CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
-	if message.content == "hello":
-		# SENDS BACK A MESSAGE TO THE CHANNEL.
-	    await message.channel.send("Yo bg")
+    username = str(message.author).split('#')[0]
 
+    if message.content.lower() == "!hello":
+        await message.channel.send(f'Coucou {username} ! UwU')
+    elif message.content.lower() == '!bye':
+        await message.channel.send(f'Au revoir {username} ^_^ !')
+    elif message.content.lower()== '!random':
+        response = f'Random number is : {random.randrange(1000000)}'
+        await message.channel.send(response)
 
-# EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
+    await bot.process_commands(message)
+
 bot.run(DISCORD_TOKEN)
