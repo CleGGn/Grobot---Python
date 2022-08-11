e4a5f6e99ad9262b042cc65605a00640a96278bd
Clément <gaugainc@yahoo.com>
2022-08-11 16:03:34 +0200

    Added gitignore

--- modele/BlaguesCog.py
+++ modele/BlaguesCog.py
@@ -2,17 +2,16 @@ import discord
 from discord.ext import commands
 from blagues_api import *
 import asyncio
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+BLAGUE_TOKEN = os.getenv("BLAGUE_TOKEN")
 
 class BlaguesCog(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
-        self.blagues = BlaguesAPI("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTE0ODQ1MzYzMjM0NDA2NDAiLCJsaW1pdCI6MTAwLCJrZXkiOiJKTHk4UWpmbHQ5VnQ1b2ppR2VRUkRRTzQ0NHdYVUd1RzR2RkIzSkV4VFdjeGlvMExuYSIsImNyZWF0ZWRfYXQiOiIyMDIyLTA4LTExVDA4OjQ0OjI1KzAwOjAwIiwiaWF0IjoxNjYwMjA3NDY1fQ.4BttynRZaqryYjEbLcyAj7rCwE5wYnr_5yCqhY1kDOA")
-
-
-# -------------------------------------------------------------------------------------------------------------------------------------------------------------------- #
-# ------------------------------------------------------------------------ FONCTIONS --------------------------------------------------------------------------------- #
-# -------------------------------------------------------------------------------------------------------------------------------------------------------------------- #
-
+        self.blagues = BlaguesAPI(BLAGUE_TOKEN)
 
 # -------------------------------------------------------------------------------------------------------------------------------------------------------------------- #
 # ------------------------------------------------------------------------ COMMANDS ---------------------------------------------------------------------------------- #
